a3b4d48fe10a17ed225aa0c3e3fbb99d6ee473cf
ManojKumarGogula <mr.gmanojkumar@gmail.com>
2023-05-17 08:55:53 +0530

    added styles

--- src/app/pages/homePage/home.js
+++ src/app/pages/homePage/home.js
@@ -84,12 +84,15 @@ const HomePage = () => {
         <img src={manImage} className=" w-full h-full object-fill" />
         </div>
         <div className="absolute w-[100vw] md:w-[60vw] top-[15vh] md:top-[40vh] xl:top-[40vh]  text-24 xl:text-40 bg-imgtrans text-bluebg font-700 opacity-60  p-[2vw] md:p-[6vh] xl:p-[6vh]">
+        <div className="flex flex-col items-center xl:items-start">
           <div className="flex flex-row md:flex-col xl:flex-col justify-center">
             <div>We are&nbsp;</div>
             <div className="text-redText">Extensive</div>
           </div>
-          <div className="text-8 md:text-20 xl:text-20 xl:w-[30vw] ">
+        
+          <div className="text-8 md:text-20 xl:text-20 xl:w-[30vw]">
             Helping you stand out in a crowded market
+          </div>
           </div>
         </div>
         <div className="bg-imgtrans text-bluebg pt-[10vh] pb-[2vh] ">
@@ -98,36 +101,37 @@ const HomePage = () => {
             <div className="font-900 text-20 md:text-40 xl:text-60 my-10">
               We are Extensive.
             </div>
-            <div className="w-5/6 text-16 text-justify my-10">
+            <div className="w-full md:w-5/6 xl:w-5/6 text-8 xl:text-16 text-justify mb-10">
               Amet minim mollit non deserunt ullamco est sit aliqua dolor do
               amet sint. Velit officia consequat duis enim velit mollit.
               Exercitation veniam consequat sunt nostrud amet.
             </div>
-            <div className="bg-bluebg p-[1vw] relative w-[80%] md:w-[25vw] xl:w-[20vw] text-imgtrans text-center rounded-8 my-10 hover:bg-imgtrans hover:text-blackText">
+            <div className="bg-bluebg p-[1vw] relative w-[80%] md:w-[25vw] xl:w-[20vw] text-imgtrans text-center rounded-4 xl:rounded-8 my-10 hover:bg-redText hover:text-imgtrans">
               BOOK A FREE MARKETING AUDIT
             </div>
           </div>
           <div className="flex flex-col md:flex-row xl:flex-row  justify-around my-20">
-            <div className="bg-roundedboxbg p-[3vw] rounded-tl-[10vw] ">
-              <div className="font-700 text-40">
+            <div className="bg-roundedboxbg p-[3vw] xl:rounded-tl-[10vw] ">
+              <div className="font-700 text-24 md:text-36 xl:text-40">
                 WHAT
                 <div>WE DO.</div>
               </div>
-              <div className="w-full xl:w-3/6">
+              <div className="w-full text-8 xl:text-16 xl:w-3/6">
                 We do lots of stuffs , basically adding value to your product .
               </div>
             </div>
-            <div className="text-20 md:text-24 xl:text-36">
+            <div className="ml-[4vw]  xl:mr-[0vw] text-20 md:text-24 xl:text-36">
               <div>Digital Marketing </div>
               <div>Digital Marketing </div>
 
               <div>Digital Marketing </div>
 
               <div>Digital Marketing </div>
+              <button className="bg-bluebg text-imgtrans text-12 p-3 px-8 rounded-4 md:rounded-8 xl:rounded-8 hover:bg-redText">View All</button>
             </div>
           </div>
           <div className="bg-bluebg">
-            <div className="w-5/6 mx-auto">
+            <div className="w-5/6 mx-auto py-[2vh]">
               <div className="text-28 text-imgtrans font-700 font-serif my-20">
                 Our <div>Work.</div>
               </div>
@@ -136,14 +140,14 @@ const HomePage = () => {
                   return (
                     <div
                       key={index}
-                      className=" w-full md:w-[30vw] xl:w-[35vw] m-20 text-imgtrans"
+                      className=" w-full md:w-[30vw] xl:w-[35vw] my-20 md:m-20 xl:m-20 text-imgtrans"
                     >
                       <img src={item.img} className="w-full object-cover" />
-                      <div className="text-redText text-12">
+                      <div className="text-redText text-8 md:text-10 xl:text-12 my-4">
                         DIGITAL MARKETING
                       </div>
-                      <div className="text-24">{item.title}</div>
-                      <div className="text-14">{item.sub}</div>
+                      <div className="text-18 md:text-20 xl:text-24">{item.title}</div>
+                      <div className="text-10 md:text-12 xl:text-14">{item.sub}</div>
                     </div>
                   );
                 })}
@@ -174,16 +178,16 @@ const HomePage = () => {
               <div className="text-bluebg font-700 text-20 md:text-56">
                 Testimonials
               </div>
-              <div className="w-[80vw] border-border shadow-10 border-1 rounded-8 flex flex-row items-center my-[2vh]">
+              <div className="w-[80vw] border-border shadow-10 border-1 rounded-8 flex flex-row items-center my-[4vh]">
                 <img src={manIcon} className=" w-[20vw] xL:w-[10vw] ml-[2vw]" />
                 <div className="flex flex-col">
-                  <span className="font-400 text-12 md:text-40 xl:text-40">
+                  <span className="font-400 text-12 md:text-40 xl:text-36">
                     “Working with Extensive is great”
                   </span>
                   <div className="flex flex-row items-center">
                     <div className="h-2 w-[10vw] bg-bluebg ml-auto mr-[2vw]"></div>
                     <div>
-                      <div className="text-12 md:text-56 xl:text-56">
+                      <div className="text-12 md:text-24 xl:text-28">
                         Hershel
                       </div>
                       <div className="text-redText text-10 w-4/6">
